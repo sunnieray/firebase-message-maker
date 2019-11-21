fa5c56c0ff724990279a295da30bc9cb95b46adc
sunnieray <sunnieray1@gmail.com>
2019-11-20 19:14:13 -0600

    add image to home page

---
+++ js/config/firebase.js
@@ -0,0 +1,6 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/database';
+import 'firebase/auth';
+import 'firebase/storage';
+
---
+++ js/index.js
@@ -0,0 +1,25 @@
+import Header from './components/Header';
+import Home from './components/Home';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.css';
+import '@fortawesome/fontawesome-free/css/all.css';
+
+pageBuild();
+
+function pageBuild() {
+	renderHeader();
+}
+
+function renderHeader() {
+	const header = document.querySelector('.header');
+	header.innerHTML = Header();
+}
+
+function renderHome() {
+	const home = document.querySelector('.nav-list__home');
+	home.addEventListener('click', () => {
+	const main = document.querySelector('.main');
+	main.innerHTML = Home();
+});
+
+}
